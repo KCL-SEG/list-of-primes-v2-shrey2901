6d5fd459082b0bfd9c428d86ef26ba561d9074b8
Shrey <shreyansh290103@outlook.com>
2023-10-12 14:02:31 +0100

    Add Value Error when value is less than 1
    Add the Vlaue error less than 1 because, numbers less than or equal to 0 is not accepted. I do agree that it might cause an issue in logic and <=0 should have been used. Note: Dangling if else issue- it will not cause any problem in this case as the rest of the program is only executed when there is no value Error. raising a value error halts the entire program.

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(num):
+    count = 0
+    for i in range(2,int(num/2)+1,1):
+        if num % i == 0:
+            count += 1
+    return count
+
+
 def primes(number_of_primes):
     list = []
+    count = 0
+    i = 1
+    if number_of_primes <1:
+        raise ValueError("The number of primes should be greater than zero")
+    while count< number_of_primes:
+        i += 1
+        if isPrime(i) < 1:
+            list.insert(count,i)
+            count += 1
     return list
